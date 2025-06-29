c6e5a949f1350dd4de1c71fa271bee3e462077b1
Javakhir <jumanovjavohir49@gmail.com>
2025-06-29 19:41:14 +0500

    payme integration implemented funktions

--- api/config/database.js
+++ api/config/database.js
@@ -62,3 +62,43 @@ if (process.env.DB_URL) {
 }
 
 module.exports = sequelize;
+/**
+ * PAY
+ */
+const pay = async (req, res) => {
+    try {
+        const { method, params, id } = req.body;
+
+        switch (method) {
+            case PaymeMethod.CheckPerformTransaction: {
+                await transactionService.checkPerformTransaction(params, id);
+                return res.json({ result: { allow: true } });
+            }
+            case PaymeMethod.CheckTransaction: {
+                const result = await transactionService.checkTransaction(params, id);
+                return res.json({ result, id });
+            }
+            case PaymeMethod.CreateTransaction: {
+                const result = await transactionService.createTransaction(params, id);
+                return res.json({ result, id });
+            }
+            case PaymeMethod.PerformTransaction: {
+                const result = await transactionService.performTransaction(params, id);
+                return res.json({ result, id });
+            }
+            case PaymeMethod.CancelTransaction: {
+                const result = await transactionService.cancelTransaction(params, id);
+                return res.json({ result, id });
+            }
+            case PaymeMethod.GetStatement: {
+                const result = await transactionService.getStatement(params, id);
+                return res.json({ result: { transactions: result } });
+            }
+        }
+    } catch (error) {
+        const statusCode = error.statusCode || 422;
+        const response = { error: error.message };
+
+        res.status(statusCode).json(response);
+    }
+};
--- api/enum/transaction.enum.js
+++ api/enum/transaction.enum.js
@@ -26,3 +26,92 @@ exports.ClickAction = {
 	Prepare: 0,
 	Complete: 1,
 }
+
+/**
+ * PAYME
+ */
+exports.PaymeMethod = {
+	CheckPerformTransaction: 'CheckPerformTransaction',
+	CheckTransaction: 'CheckTransaction',
+	CreateTransaction: 'CreateTransaction',
+	PerformTransaction: 'PerformTransaction',
+	CancelTransaction: 'CancelTransaction',
+	GetStatement: 'GetStatement',
+}
+exports.PaymeError = {
+	InvalidAmount: {
+		name: 'InvalidAmount',
+		code: -31001,
+		message: {
+			uz: "Noto'g'ri summa",
+			ru: 'Недопустимая сумма',
+			en: 'Invalid amount',
+		},
+	},
+	UserNotFound: {
+		name: 'UserNotFound',
+		code: -31050,
+		message: {
+			uz: 'Biz sizning hisobingizni topolmadik.',
+			ru: 'Мы не нашли вашу учетную запись',
+			en: "We couldn't find your account",
+		},
+	},
+	BookingNotFound: {
+		name: 'BookingNotFound',
+		code: -31050,
+		message: {
+			uz: 'Biz zakasni topolmadik.',
+			ru: 'Закаса мы не нашли.',
+			en: 'We could not find Zakas.',
+		},
+	},
+	CantDoOperation: {
+		name: 'CantDoOperation',
+		code: -31008,
+		message: {
+			uz: 'Biz operatsiyani bajara olmaymiz',
+			ru: 'Мы не можем сделать операцию',
+			en: "We can't do operation",
+		},
+	},
+	TransactionNotFound: {
+		name: 'TransactionNotFound',
+		code: -31003,
+		message: {
+			uz: 'Tranzaktsiya topilmadi',
+			ru: 'Транзакция не найдена',
+			en: 'Transaction not found',
+		},
+	},
+	AlreadyDone: {
+		name: 'AlreadyDone',
+		code: -31060,
+		message: {
+			uz: "Mahsulot uchun to'lov qilingan",
+			ru: 'Оплачено за товар',
+			en: 'Paid for the product',
+		},
+	},
+	Pending: {
+		name: 'Pending',
+		code: -31050,
+		message: {
+			uz: "Mahsulot uchun to'lov kutilayapti",
+			ru: 'Ожидается оплата товар',
+			en: 'Payment for the product is pending',
+		},
+	},
+	InvalidAuthorization: {
+		name: 'InvalidAuthorization',
+		code: -32504,
+		message: {
+			uz: 'Avtorizatsiya yaroqsiz',
+			ru: 'Авторизация недействительна',
+			en: 'Authorization invalid',
+		},
+	},
+}
+exports.PaymeData = {
+	BookingId: 'booking_id'
+}
