b80936fbe668b96dfbbfde759052cc57034bcaba
Dilik <1989deny@gmail.com>
2025-07-01 22:42:28 +0500

    clear all button

--- client/src/components/FilterRow.jsx
+++ client/src/components/FilterRow.jsx
@@ -274,7 +274,7 @@ export default function FilterRow({
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                 </svg>
-                Reset Filters
+                Reset
               </button>
             )}
           </div>
