33bd6771020a045889d5cc870b6f574daeb9d049
Dilik <1989deny@gmail.com>
2025-10-01 12:54:05 +0500

    change hte api url

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -7,7 +7,7 @@
  * - Authentication state sync
  */
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:4000";
 
 /**
  * Get user's current language preference
